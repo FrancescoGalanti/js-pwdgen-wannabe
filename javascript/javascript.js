9b57d9dfe747a0aa31da1748477bdad0c4c2407a
FrancescoGalanti <galanti_francesco>
2020-10-13 14:54:38 +0200

    javascript fatto

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,30 @@
 /*
 **javascript
 **/
+
+
+
+// prima variabile //
+ var nomeUtente = prompt('quale il tuo nome ?');
+
+
+ // seconda variabile//
+
+ var usernameUtente = prompt('quale il tuo cognome ?');
+
+ // terza varibile//
+
+ var colorUtente = prompt('quale il tuo colore preferito ?') ;
+
+ // Password//
+
+ var password = nomeUtente + usernameUtente + colorUtente;
+
+
+// output primo//
+
+
+document.getElementById('Password').innerHTML = password
+
+// output secondo//
+console.log("PASSWORD: " + password)
